83f832ac2c9c5173df9a5ec8b013cdf52f64cf37
dione24 <dioneniangaly@gmail.com>
2021-06-29 13:15:51 +0000

    dd

--- Applications/App/Modules/Bielletage/Views/bielletage.php
+++ Applications/App/Modules/Bielletage/Views/bielletage.php
@@ -281,7 +281,6 @@
                         <div class="form-group">
                             <label class="control-label">Caisse</label>
                             <select class="form-control" name="RefCaisse" tabindex="1" required="">
-                                <option>Caisse</option>
                                 <?php foreach ($CheckOuverture as $key => $Caisse) {
                                         if ($Caisse['caisse'] != $Caisse['RefCaisse']) {
                                     ?>
@@ -314,7 +313,7 @@
                         <div class="form-group">
                             <label class="control-label">Caisse</label>
                             <select class="form-control" name="RefCaisse" id="RefCaisse" tabindex="1" required="">
-                                <option>Caisse</option>
+
                                 <?php foreach ($CheckOuverture as $key => $Caisse) {
                                         if ($Caisse['caisse'] != $Caisse['RefCaisse']) {
                                     ?>
