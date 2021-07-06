b0b370e1c5bc7654fa725b9c6ca2bdae341c42b7
Niangaly Abdoulaye <dioneniangaly@gmail.com>
2021-07-06 17:14:16 +0000

    ok

--- Web/config/db.php
+++ Web/config/db.php
@@ -2,7 +2,7 @@
 ob_start();
 try {
 
-    $baseDeDonnee = new PDO('mysql:host=localhost;dbname=iob', 'root', '');
+    $baseDeDonnee = new PDO('mysql:host=localhost;dbname=iob', 'root', 'root');
     $baseDeDonnee->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $baseDeDonnee->exec('SET NAMES utf8');
 } catch (Exception $e) {
